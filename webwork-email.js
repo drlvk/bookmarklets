c5b7bae7a044037c76797b9f00ce8b68d828f0d1
drlvk <drlvk@users.noreply.github.com>
2017-09-16 16:21:05 -0400

    whitespace

--- webwork-email.js
+++ webwork-email.js
@@ -4,7 +4,7 @@ if (u) {  // Works in Gmail
   u = u.href.split("@")[0].split(":")[1];
 }
 else {    // Works in Outlook Web Access
-	u = document.querySelector('img[src*="service.svc"]');
+  u = document.querySelector('img[src*="service.svc"]');
   u = u.src.split("%")[0].split("email=")[1];
 }
 window.open(w + "?showCorrectAnswers=1&effectiveUser=" + u);
