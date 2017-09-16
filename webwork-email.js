7bf6a5bc598d41a80d3ff226b9fc972f0bc286b8
drlvk <drlvk@users.noreply.github.com>
2017-09-16 15:50:44 -0400

    Create webwork-email.js

---
+++ webwork-email.js
@@ -0,0 +1,11 @@
+var w = document.querySelector('a[href*="webwork.syr.edu"]').href;
+w = w.split("?")[0];
+var u = document.querySelector('a[href*="@syr.edu"]');
+if (u) {  // Works in Gmail 
+  u = u.href.split("@")[0].split(":")[1];
+}
+else {    // Works in Outlook Web Access
+	u = document.querySelector('img[src*="service.svc"]');
+  u = u.src.split("%")[0].split("email=")[1];
+}
+window.open(w + "?showCorrectAnswers=1&effectiveUser=" + u);
