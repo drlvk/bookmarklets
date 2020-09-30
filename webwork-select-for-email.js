9fa87e601013c6b95f1de43ff8e52a78fa6d1e01
drlvk <drlvk@users.noreply.github.com>
2020-09-30 19:57:23 -0400

    Create webwork-select-for-email.js

---
+++ webwork-select-for-email.js
@@ -0,0 +1,6 @@
+let students = document.querySelector("select.ScrollingRecordList").children;
+let entered = document.getElementById("email-body").value;
+let listSelect = entered.replace(/\s/g, ",").split(',');
+for (const s of students) {
+s.selected = (listSelect.indexOf(s.value) !== -1);
+}
