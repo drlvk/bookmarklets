e3f336b690ffa89442c1e3a5d438df03ae5bcaf5
drlvk <drlvk@users.noreply.github.com>
2021-06-01 13:24:55 -0400

    Update ww-times.js

--- ww-times.js
+++ ww-times.js
@@ -1,3 +1,3 @@
 e=document.getElementsByClassName("hasDatepicker");
-d=['at 09:00am','at 11:59pm','at 11:59pm'];
+d=['at 08:00am','at 11:59pm','at 11:59pm'];
 k=0;for (f of e) {f.value=f.value.replace(/at.*/,d[k]);k=(k+1)%3;}
