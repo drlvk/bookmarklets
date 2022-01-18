4e589bce4fb974324a5b9be9df87596923c5c8f1
drlvk <drlvk@users.noreply.github.com>
2022-01-17 19:10:26 -0500

    Update ww-dates-reduced.js

--- ww-dates-reduced.js
+++ ww-dates-reduced.js
@@ -2,3 +2,4 @@ e=document.getElementsByClassName("hasDatepicker");
 days=[6,13,13];
 times=['11:59pm','11:59pm','11:59pm'];
 k=0;for (f of e) {if (k==0) {fv=f.value} else {x=new Date(fv.split(' ')[0]); x.setDate(x.getDate()+days[k-1]); f.value=(x.getMonth()+1)+'/'+x.getDate()+'/'+x.getFullYear()+' at '+times[k-1]} k=(k+1)%4}
+ 
