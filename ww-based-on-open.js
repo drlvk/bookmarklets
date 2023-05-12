22d83edcd8f2723874988fc5df9494c409a2e157
drlvk <drlvk@users.noreply.github.com>
2023-05-12 15:38:28 -0400

    Create ww-based-on-open.js

---
+++ ww-based-on-open.js
@@ -0,0 +1,12 @@
+// Set due and answer dates by based on the open dates
+e=document.getElementsByClassName("hasDatepicker");
+for (k=0;k<e.length;k++) {
+if (k%3==0) {
+m=e[k].value.match(/^(\d{2})\/(\d{2})\/(\d{4})(.*)$/);
+m[4]=' at 11:59pm';
+d=new Date(m[3],m[1]-1,m[2]);
+d.setDate(d.getDate()+7);
+e[k+1].value=d.toLocaleDateString()+m[4];
+d.setDate(d.getDate()+1);
+e[k+2].value=d.toLocaleDateString()+m[4];
+}}
