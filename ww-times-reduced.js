1f17c22f6f53e1bdfd68927481facfdfd7adff46
drlvk <drlvk@users.noreply.github.com>
2024-08-24 18:55:31 -0400

    Update ww-times-reduced.js

--- ww-times-reduced.js
+++ ww-times-reduced.js
@@ -1,3 +1,7 @@
-e=document.getElementsByClassName("hasDatepicker");
-d=['at 08:00am','at 11:59pm','at 11:59pm','at 11:59pm'];
-k=0;for (f of e) {f.value=f.value.replace(/at.*/,d[k]);k=(k+1)%4;}
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms));}
+async function main() {
+e=document.getElementsByClassName("form-control input");
+d=[', 8:00 AM',', 11:59 PM',', 11:59 PM',', 11:59 PM'];
+k=0;for (f of e) {f.value=f.value.replace(/, .*/,d[k]); f.focus(); k=(k+1)%4; await sleep(500); }
+}
+main();
